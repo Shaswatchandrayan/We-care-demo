0b4c5e19a78bbebd76d372fb4774ad9035e2b5f3
Chandrayan <schandr11@TAGICSUs-MacBook-Air-3.local>
2024-02-27 12:45:19 +0530

    second-commit

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <Navbar/>
+    <h1>Hello from Home</h1>
+</template>
+<script>
+import Navbar from './Navbar.vue';
+
+export default {
+    name: "HomePage", 
+    components:{
+        Navbar,
+    }
+}
+  
+</script>
